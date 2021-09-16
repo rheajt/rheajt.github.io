6829476d976da633fb37c7c11663b92c5b60e72e
Jordan Rhea <rheajt@gmail.com>
2021-09-16 10:20:18 +0800

    added a post; updatd the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,6 +5,9 @@ const NotFoundPage = () => (
     <Layout>
         <Seo title="Page not found" />
         <h1>NOT FOUND</h1>
+        <p>Going to be honest with you... I probably broke it.</p>
+
+        <p>Let me know on <a href="https://twitter.com/rheajt">my twitter page @rheajt</a></p>
     </Layout>
 );
 
