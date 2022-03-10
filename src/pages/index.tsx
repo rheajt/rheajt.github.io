5f6259ae66972f317e75b201934cd85d05c2f5da
jordan rhea <rheajt@gmail.com>
2022-03-10 14:15:02 +0800

    updated the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import * as React from "react"
-import { graphql } from "gatsby"
+import * as React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import { StaticImage } from "gatsby-plugin-image";
 
-const Home: React.FC<{ data: any; location: any; }> = ({ data, location }) => {
-    const siteTitle = data.site.siteMetadata?.title || `Title`
+const Home: React.FC<{ data: any; location: any }> = ({ data, location }) => {
+    const siteTitle = data.site.siteMetadata?.title || `Title`;
 
     return (
         <Layout location={location} title={siteTitle}>
@@ -24,22 +24,32 @@ const Home: React.FC<{ data: any; location: any; }> = ({ data, location }) => {
                 />
             </div>
 
-            <p style={{ margin: `2rem 0 1rem 0` }}>My name is</p>
-            <h1 style={{ margin: `0 0 2rem` }}>Jordan Rhea</h1>
-            <p>I use web technology to build solutions to problems. Primarily written in Typescript to automate tasks that slow people down.</p>
-            <p>This site is primarily a scratch pad for me to jot things down as I learn them, do them, or build them. I also used to <a href="https://youtube.com/jordanrhea">post things on YouTube.</a></p>
+            <h2 style={{ margin: `0 0 2rem` }}>Jordan Rhea</h2>
+
+            <p>
+                Data and Systems Developer. I am using web technologies to build
+                custom solutions using both Google and Microsoft clouds.
+            </p>
+
+            <p>
+                This site is primarily a scratch pad for me to jot things down
+                as I learn them, do them, or build them. I also used to{" "}
+                <a href="https://youtube.com/jordanrhea">
+                    post things on YouTube.
+                </a>
+            </p>
         </Layout>
-    )
-}
+    );
+};
 
 export default Home;
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
     }
-  }
-`
+`;
