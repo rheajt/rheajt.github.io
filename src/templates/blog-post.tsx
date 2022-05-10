6915c136468e6fabb9c0e40f9660a064addc65b1
jordan rhea <rheajt@gmail.com>
2022-05-10 19:10:30 +0800

    feat: adding more disqus stuff

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -102,6 +102,7 @@ const BlogPostTemplate: React.FC<Props> = ({ data, location }) => {
                     <Bio />
                 </footer>
             </article>
+
             <nav className="blog-post-nav">
                 <ul
                     style={{
@@ -114,14 +115,17 @@ const BlogPostTemplate: React.FC<Props> = ({ data, location }) => {
                 >
                     <li>
                         {previous && (
-                            <Link to={previous.fields.slug} rel="prev">
+                            <Link
+                                to={"/blog/" + previous.fields.slug}
+                                rel="prev"
+                            >
                                 ← {previous.frontmatter.title}
                             </Link>
                         )}
                     </li>
                     <li>
                         {next && (
-                            <Link to={next.fields.slug} rel="next">
+                            <Link to={"/blog/" + next.fields.slug} rel="next">
                                 {next.frontmatter.title} →
                             </Link>
                         )}
